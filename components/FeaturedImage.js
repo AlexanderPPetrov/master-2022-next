16709cd89c5c1bceb38c40e6f302a3bfc0387032
Aleksander Petrov <apetrovdev@gmail.com>
2022-01-23 11:59:50 +0200

    next image example

--- components/FeaturedImage.js
+++ components/FeaturedImage.js
@@ -1,10 +1,18 @@
-import {Image} from 'react-bootstrap';
+import Image from 'next/image'
 
 function FeaturedImage() {
     return (
-        <Image thumbnail={true}
-         src="https://previews.123rf.com/images/piksel/piksel1506/piksel150600089/41508771-familia-feliz-al-aire-libre.jpg"/>
+        <Image
+            className="img-thumbnail"
+            layout="responsive"
+            src="https://previews.123rf.com/images/piksel/piksel1506/piksel150600089/41508771-familia-feliz-al-aire-libre.jpg"
+            alt="Featured image"
+            width={795}
+            height={554}
+      />
     )
 }
 
 export default FeaturedImage;
+
+
--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
   i18n,
   reactStrictMode: true,
+  images: {
+    domains: ['previews.123rf.com'],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
 };
